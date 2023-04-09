f515f2421ac688399c416d92eb1a2b09cfb5f7d7
Abhilash4597 <abhilash4597@gmail.com>
2023-04-10 01:03:23 +0530

    css for expense date updated

--- src/Components/ExpenseDate.jsx
+++ src/Components/ExpenseDate.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import './ExpenseDate.css'
 
 export default function ExpenseDate(props) {
 
   let day = props.date.getDay();
-  let month = props.date.getMonth();
+  let month = props.date.toLocaleString('default', { month: 'long' });
   let year = props.date.getFullYear();
   
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date_month">{month}</div>
+      <div className="expense-date_year">{year}</div>
+      <div className="expense-date_day">{day}</div>
     </div>
   );
 }
