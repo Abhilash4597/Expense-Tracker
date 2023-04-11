886d191d1f5228fcb2a53cc56ba69baab96f6977
Abhilash4597 <abhilash4597@gmail.com>
2023-04-11 09:45:28 +0530

    folder structure updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Expenses from './Components/Expenses';
+import Expenses from '../public/CSS/JSX/Expenses';
 
 export default function App() {
   const expenses = [
